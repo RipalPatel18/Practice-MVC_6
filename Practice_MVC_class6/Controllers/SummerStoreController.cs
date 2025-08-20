6232cc29acb481112e65676795430ea4ff972bd4
RipalPatel18 <n01578468@humber.ca>
2025-08-20 16:52:07 -0400

    Initial commit

---
+++ Practice_MVC_class6/Controllers/SummerStoreController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Practice_MVC_class6.Controllers
+{
+    public class SummerStoreController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Order()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CheckOut(string CustomerName, int NumPopscicles, string IceCreamFlavour)
+        {
+            Debug.WriteLine("I have received data from a POST request");
+            Debug.WriteLine(CustomerName);
+            Debug.WriteLine(NumPopscicles);
+            Debug.WriteLine(IceCreamFlavour);
+
+            ViewData["CustomerName"] = CustomerName;
+            ViewData["NumPopscicles"] = NumPopscicles;
+            ViewData["IceCreamFlavour"] = IceCreamFlavour;
+
+            decimal PopsciclesCost = 1.5M;
+
+            decimal SubTotal = NumPopscicles * PopsciclesCost;
+            decimal Taxrate = 0.13M;
+            decimal HST = SubTotal * Taxrate;
+
+            ViewData["SubTotal"] = SubTotal;
+            ViewData["HstAmt"] = HST;
+            ViewData["HstRate"] = "13%";
+            ViewData["OrderTotal"] = SubTotal + HST;
+
+
+            return View();
+        }
+    }
+}
